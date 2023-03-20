99ec268e0f26501b2b85d3244888849082972e34
IFedor1984I <yandexfedia1984@mail.ru>
2023-03-21 00:51:26 +0300

    profi3

--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,21 @@ import * as flsFunctions from './modules/functions.js';
 flsFunctions.isWebp();
 
 // Слайдер
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// const swiper = new Swiper();
 
-import $ from 'jquery';
-import  'slick-carousel';
+// import $ from 'jquery';
+// import  'slick-carousel';
 
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000
-});
+// $('.carousel').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 1000
+// });
       
 
 
